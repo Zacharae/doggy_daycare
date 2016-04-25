1730d37f9f7c4d1deaf2907185020ddc7db4d072
Zacharae <zacharyxae@gmail.com>
2016-04-25 10:18:27 -0500

    edit functionality

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -23,11 +23,21 @@ class DogsController < ApplicationController
 	def edit
 		@dog = Dog.find(params[:id])
 	end
+
+	def update
+		@dog = Dog.find(params[:id])
+		if @dog.update(dog_params)
+			redirect_to dogs_url, notice: "Successfully updated!"
+		else
+			flash.now[:notice] = "something went wrong, didn't update your dog."
+			render :edit
+		end
+	end
 end
 
 private
 	def dog_params
-		params.require(:dog) .permit(:name, :breed, :age)		
+		params.require(:dog).permit(:name, :breed, :age)		
 	end
 	
 end
